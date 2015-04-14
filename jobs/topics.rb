d3f79455220811554a07057af20b44159738a125
yada-ITA <yada_keiko@ogis-ri.co.jp>
2015-04-14 19:07:52 +0900

    kilogyのJSONからデータ取得するようにした。

---
+++ jobs/topics.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+
+# proxy設定
+proxy = ENV["PROXY_PATH"]
+proxy_user = ENV["PROXY_ID"]
+proxy_pass = ENV["PROXY_PW"]
+options = { proxy_http_basic_authentication: [proxy, proxy_user, proxy_pass] }
+
+
+# URL
+url = 'http://kilogy-test.herokuapp.com/topics.json'
+
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+SCHEDULER.every '10s', :first_in => 0 do |job|
+
+buffer = open(url,options).read
+json_data =  JSON.parse(buffer)
+
+
+  send_event('topics', { items: json_data})
+
+
+end
