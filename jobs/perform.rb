b514e4cffcaedd7c8da6c2e36f33d768ca0e4242
yada-ITA <yada.ita+github@gmail.com>
2015-06-05 08:05:53 +0000

    201506のデータを取得

--- jobs/perform.rb
+++ jobs/perform.rb
@@ -14,7 +14,7 @@ base_url = ENV["TORA_JISSEKI_URL"]
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  ym = "201505"
+  ym = "201506"
   url = base_url + ym
 
   buffer = open(url,options).read
@@ -24,14 +24,6 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
 
   progress_calc = (dat[2].to_f/dat[3] * 100).round
  
-  puts "******json_data************#########################***************************"
-  puts json_data
-  puts "****dat******************#########################***************************"
-  puts dat
-  puts "****progress_calc********#########################***************************"
-
-  puts progress_calc
-puts "**********************#########################***************************"
 
   send_event('perform', { ok: dat[0], ng: dat[1], 
                          complete: dat[2], yotei: dat[3],
